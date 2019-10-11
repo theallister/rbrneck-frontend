55e9d7df0386f6e3f36ffbcf695a469edaf872b3
gustafedlund <38979774+gustafedlund@users.noreply.github.com>
2019-10-11 15:24:15 +0200

    w

--- src/rbrneck-client.js
+++ src/rbrneck-client.js
@@ -4,6 +4,47 @@ const jwtDecode = require('jwt-decode') //när man skall decoda tokens senare...
 const request = new XMLHttpRequest()
 const uri = "http://localhost:3000"
 
+exports.login = function(username, password, callback) {
+
+  request.open("POST", uri+"/tokens")
+  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+  request.send("grant_type=password&username="+encodeURIComponent(username)+"&password="+encodeURIComponent(password))
+
+  request.addEventListener("load", () => {
+
+    const status = request.status
+
+    switch(status) {
+
+      case 200:
+        var bodyAsString = request.responseText
+        var body = JSON.parse(bodyAsString)
+
+        const idToken = body.id_token
+        accessToken = body.access_token
+
+        const userInfo = jwtDecode(idToken)
+        const id = userInfo.accountId
+        const username = userInfo.accountUsername
+
+        callback([], id, username, idToken, accessToken)
+        break
+
+      case 400:
+        var bodyAsString = request.responseText
+        var body = JSON.parse(bodyAsString)
+        callback([body.error])
+        break
+
+      case 500:
+        callback(["Unknown server error"])
+        break
+
+      default:
+        callback(["Unknown server error"])
+    }
+  })
+}
 
 exports.createAccount = function(account, callback) { //Create an account based on object from .vue
 
@@ -82,6 +123,9 @@ exports.getAccountById = function(id, callback) { //fetch one specific account f
         const account = JSON.parse(bodyAsString)
         callback([], account)
         break
+      case 404:
+        callback(["Account not found"])
+        break
       case 500:
         callback(["Server error"])
         break
@@ -93,7 +137,7 @@ exports.getAccountById = function(id, callback) { //fetch one specific account f
 
 exports.updateAccountById = function(id, updatedAccount, callback) {
 
-  request.open("PATCH", uri+"/accounts/"+id)
+  request.open("PUT", uri+"/accounts/"+id)
   request.setRequestHeader("Content-Type", "application/json")
   request.send(JSON.stringify(updatedAccount))
 
@@ -105,11 +149,101 @@ exports.updateAccountById = function(id, updatedAccount, callback) {
       case 200:
         callback([])
 
+      case 404:
+        callback(["Account not found"])
+        break
+
+      case 422:
+        callback(["Unprocessable entry"])
+        break
+
       case 500:
         callback(["Server error"])
-        
+
       default:
         callback(["Server error"])
+    }
+  })
+}
+
+exports.deleteAccountById = function(id, callback) {
+
+  request.open("DELETE", uri+"/accounts/"+id)
+  request.send()
+
+  request.addEventListener("load", () => {
+    const status = request.status
+
+    switch(status) {
+
+      case 200:
+        const bodyAsString = response.responseText
+        const items = JSON.parse(bodyAsString)
+        callback([], items)
+
+      case 404:
+        callback(["Account not found"])
+        break
+
+      case 500:
+        callback(["Server error"])
+        break
+
+      default:
+        callback(["Server error"])
+    }
+  })
+}
+exports.getAllItems = function(callback) {
+
+  request.open("GET", uri+"/items")
+  request.send()
+
+  request.addEventListener("load", () => {
+
+    const status = request.status
+
+    switch(status) {
+
+      case 200:
+        callback([], items)
+
+      case 500:
+        callback(["Server error"])
+        break
+
+      default:
+
+    }
+
+  })
+
+}
+
+
+exports.createItem = function(item, callback) {
+
+  request.open("POST", uri+"/items")
+  request.setRequestHeader("Content-Type", "application/json") //Header inställd för JSON objekt
+  request.send(JSON.stringify(item))
+
+  request.addEventListener("load", () => {
+    const status = request.status
+
+    switch(status) {
+      case 200:
+      const location = request.getResponseHeader("Location") //Vi vill ha ID:t och inte hela pathen
+      const id = parseInt(location.substr("/items/".length)) //Genom att göra detta (ta bort stringen före numret)
+      callback([], id) //sedan skickar vi id:t och en tom errors array
+      break
+
+      case 500:
+      callback(["Server error"])
+      break
+
+      default:
+      callback(["Server error"])
+
     }
   })
 }
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -4,6 +4,9 @@
     <button @click="getAllAccounts()">Get All Accounts</button>
     <button @click="getAccountById()">Get Account By Id</button>
     <button @click="updateAccountById()">Update Account By Id</button>
+    <button @click="deleteAccountById()">Delete Account By Id</button>
+    <button @click="createItem()">Create Item</button>
+    <button @click="login()">Login</button>
   </div>
 </template>
 
@@ -19,14 +22,33 @@ export default {
     return {
       username: "HardcodedName12",
       password: "HardcodedPassword12",
-      id: 1,
+      id: 3,
       errors: [],
       accounts: [],
       account: [],
-      success: Boolean
+      success: Boolean,
+      accessToken: "",
+      idToken: "",
+      loggedInUser: ""
     }
   },
   methods: {
+    login() {
+      let credentials = {
+        username: this.username,
+        password: this.password
+      }
+      client.login(credentials.username, credentials.password, (errors, id, username, idToken, accessToken) => {
+        if (errors.length == 0) {
+          this.success = true
+          this.idToken = idToken
+          this.accessToken = accessToken
+          this.loggedInUser = username
+        } else {
+          this.errors = errors
+        }
+      })
+    },
     createAccount() {
 
       let account = {
@@ -72,6 +94,35 @@ export default {
       }
 
       client.updateAccountById(id, updatedAccount, (errors) => {
+        if (errors.length == 0) {
+          this.success = true
+        } else {
+          this.errors = errors
+        }
+      })
+    },
+    deleteAccountById() {
+      let id = this.id
+
+      client.deleteAccountById(id, (errors) => {
+        if (errors.length == 0) {
+          this.success = true
+        } else {
+          this.errors = errors
+        }
+      })
+    },
+    createItem() {
+      let item = {
+        accountId: this.id,
+        title: "My Little Pony",
+        series: 0,
+        season: null,
+        episode: null,
+        watched: 0
+      }
+
+      client.createItem(item, (errors, id) => {
         if (errors.length == 0) {
           this.success = true
         } else {
